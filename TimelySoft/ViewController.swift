3354784afa31233ee64bbbcac06ae2cc3e85dadc
Ostep <ostep1@icloud.com>
2021-05-06 15:02:26 +0600

    Initial Commit

---
+++ TimelySoft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimelySoft
+//
+//  Created by Рамазан Юсупов on 5/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
